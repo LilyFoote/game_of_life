6187d17b537594117051a0291c9139801b51d933
Ian Foote <ian@feete.org>
2013-08-10 12:44:46 +0100

    Add "Remove cells" and "Clear" buttons.

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ from kivy.uix.scatter import ScatterPlane
 
 import life
 
+BLACK = (0, 0, 0)
 BLUE = (0, 0, 1)
 CYAN = (0, 1, 1)
 
 class LifeBoard(ScatterPlane):
     cells = ObjectProperty(Counter())
+    dead_colour = ListProperty(BLACK)
     alive_colour = ListProperty(BLUE)
     aged_cell_colour = ListProperty(CYAN)
 
     cell_width = NumericProperty(10)
     cell_size = ReferenceListProperty(cell_width, cell_width)
     draw = BooleanProperty(False)
+    erase = BooleanProperty(False)
 
     def update_cells(self):
         self.cells = life.next_iteration(self.cells)
@@ -43,25 +46,41 @@ class LifeBoard(ScatterPlane):
     def toggle_draw(self, value):
         self.draw = value == 'down'
 
+    def toggle_erase(self, value):
+        self.erase = value == 'down'
+
+    def on_touch_down(self, touch):
+        if self.draw or self.erase:
+            self.edit_cells(touch)
+        else:
+            super(LifeBoard, self).on_touch_down(touch)
+
     def on_touch_move(self, touch):
-        if self.draw:
-            cells = self.cells
-            cell_width = self.cell_width
-
-            x = touch.x - self.x
-            y = touch.y - self.y
-
-            pos = (int(x//cell_width), int(y//cell_width))
-
-            if not cells[pos]:
-                cells[pos] = 1
-                self.canvas.add(Color(*self.alive_colour))
-                cell_pos = (pos[0]*cell_width, pos[1]*cell_width)
-                self.canvas.add(Rectangle(size=self.cell_size, pos=cell_pos))
-
-            self.cells = cells
+        if self.draw or self.erase:
+            self.edit_cells(touch)
         else:
             super(LifeBoard, self).on_touch_move(touch)
+
+    def edit_cells(self, touch):
+        cells = self.cells
+        cell_width = self.cell_width
+
+        x = touch.x - self.x
+        y = touch.y - self.y
+
+        pos = (int(x//cell_width), int(y//cell_width))
+        cell_pos = (pos[0]*cell_width, pos[1]*cell_width)
+
+        if self.draw and not cells[pos]:
+            cells[pos] = 1
+            self.canvas.add(Color(*self.alive_colour))
+        elif self.erase and cells[pos]:
+            cells[pos] = 0
+            self.canvas.add(Color(*self.dead_colour))
+
+        self.canvas.add(Rectangle(size=self.cell_size, pos=cell_pos))
+
+        self.cells = cells
 
 class LifeApp(App):
     pass
